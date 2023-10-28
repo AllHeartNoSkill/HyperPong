f6dd4bc47297202d6e74cefb448d8179089abdd4
peculiarnewbie <32804415+peculiarnewbie@users.noreply.github.com>
2023-10-28 14:13:03 +0700

    switch from rigidbody to spherecast

--- Assets/Scripts/Ball/BallMovement.cs
+++ Assets/Scripts/Ball/BallMovement.cs
@@ -8,7 +8,8 @@ public class BallMovement : MonoBehaviour
 
     [SerializeField] private float baseSpeed = 1f; // shouldn't be here
     [SerializeField] private Vector3 ballDirection = new Vector3(1, 0, 0);
-    
+    [SerializeField] private float castRadius = 1f;
+
     private Transform ballTransform;
     private Rigidbody _rigidBody;
     private GameObject _lastCollidedObject;
@@ -26,8 +27,8 @@ public class BallMovement : MonoBehaviour
 
     void Update()
     {
-        // MoveBall();
-
+        CheckForCollision();
+        MoveBall();
     }
 
     [ContextMenu("Reset Ball")]
@@ -37,54 +38,27 @@ public class BallMovement : MonoBehaviour
         ballDirection = new Vector3(1, 0, 0);
     }
 
-    private void FixedUpdate()
-    {
-        // ballTransform.position += baseSpeed * Time.fixedDeltaTime * ballDirection;
-        _rigidBody.velocity = baseSpeed * ballDirection;
-    }
-
     void MoveBall(){
         // determine speed modifier from direction
         float speedModifier = 1f;
 
-        // Vector3 temp = ballTransform.position;
-        // temp += 
-
         ballTransform.position += baseSpeed * speedModifier  * Time.deltaTime * ballDirection;
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if(other.gameObject == _lastCollidedObject) return;
-        _lastCollidedObject = other.gameObject;
-        
-        if(other.gameObject.tag == "Bounce Wall"){
-            ReflectBall(other);
+    void CheckForCollision(){
+        RaycastHit hit;
+
+        Vector3 p1 = transform.position;
+        float distanceToObstacle = 0;
+
+        if (Physics.SphereCast(p1, castRadius / 2, ballDirection, out hit, 10))
+        {
+            distanceToObstacle = hit.distance;
+            if(distanceToObstacle < castRadius){
+                ballDirection = Vector3.Reflect(ballDirection, hit.normal);
+                Debug.Log("hit " + ballDirection);
+
+            }
         }
-    }
-
-    private void OnCollisionStay(Collision other) {
-        if(other.gameObject == _lastCollidedObject) return;
-        _lastCollidedObject = other.gameObject;
-        
-        if(other.gameObject.tag == "Bounce Wall"){
-            ReflectBall(other);
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        _lastCollidedObject = null;
-    }
-
-    private void ReflectBall(Collision other)
-    {
-        ContactPoint[] contacts = other.contacts;
-        
-        Debug.Log("hit " + contacts[0].normal);
-        ballDirection = Vector3.Reflect(ballDirection, contacts[0].normal);
-        ballDirection.z = 0f;
-        Debug.Log("newDie " + ballDirection);
-
     }
 }
