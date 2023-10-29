d111cef542edee5d754aa3e17c6607142ff4c038
peculiarnewbie <32804415+peculiarnewbie@users.noreply.github.com>
2023-10-29 23:48:19 +0700

    ui fixes

--- Assets/CardUIParent.cs
+++ Assets/CardUIParent.cs
@@ -4,6 +4,7 @@ using BrunoMikoski.AnimationSequencer;
 using UnityEngine;
 using UnityEngine.InputSystem.UI;
 using BrunoMikoski.AnimationSequencer;
+using TMPro;
 
 public class CardUIParent : MonoBehaviour
 {
@@ -15,12 +16,19 @@ public class CardUIParent : MonoBehaviour
 
     [SerializeField] PlayerType player;
 
+    [SerializeField] TMP_Text titleText;
+    [SerializeField] TMP_Text activeText;
+    [SerializeField] TMP_Text passiveText;
+
     // [SerializeField] int powerCardId;
     public PowerCard powerCard;
 
     public void InitCard(int id) {
         powerCard = PowerReference.instance.powerCards[id];
         cardUI.AssignImage(powerCard.cardSprite);
+        titleText.text = powerCard.id;
+        activeText.text = powerCard.activeDescription;
+        passiveText.text = powerCard.passiveDescription;
     }
     
     public void SelectCard(){
